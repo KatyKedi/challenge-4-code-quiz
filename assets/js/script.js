0825da82606fbf4fbd456422a194160791d19446
KatyKedi <katykat5bros@gmail.com>
2022-01-24 16:55:51 -0800

    timer countsdown and stops at zero

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 var pageContentEl = document.querySelector("#page-content");
 var promptEl = document.querySelector(".prompt");
+var timer = document.querySelector(".timer");
+var timerInt = 10;
 
 var submitButtonHandler = function(event) {
     event.preventDefault();
     //get target element from event
     var targetEl = event.target;
 
-    //if submit button was clicked, run resetPage
+    //if submit button was clicked, reset the page and start the clock
     if (targetEl.matches(".btn")) {
         resetPage();
+        startTimer();
     }
 };
 
@@ -17,8 +20,25 @@ var resetPage = function() {
     promptEl.remove();
 };
 
+var startTimer = function() {
+    //timer starts at 120 seconds and counts down until 0
+    var decrementTimer = function() { 
+        //convert the integer to a string
+        timerString = JSON.stringify(timerInt);
+        //add the count after "Time: "
+        timer.textContent = "Time: " + timerString;
+        console.log("still going");
+        if (timerInt > 0) {
+            timerInt--;
+        }
+        else {
+            clearInterval(timerInterval);
+        }
+    };
 
+    //run the function on an interval
+    var timerInterval = setInterval(decrementTimer, 1000);
+};
 
 //listen for the start button click
 pageContentEl.addEventListener("click", submitButtonHandler);
-
